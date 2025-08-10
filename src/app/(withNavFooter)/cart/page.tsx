e90bfe3feca4e4960f6478044b303a16e936517a
Rabeya Akter <rabeyariya251@gmail.com>
2025-08-10 11:00:41 +0600

    implement checkout page

--- src/app/(withNavFooter)/cart/page.tsx
+++ src/app/(withNavFooter)/cart/page.tsx
@@ -1,11 +1,133 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import image from "../../../assets/image/Rectangle 161.png";
+import Link from "next/link";
 
 const CartPage = () => {
-  return <div>
+  const [cartItems, setCartItems] = useState([
+    {
+      id: 1,
+      name: "GEEK VAPE H1 POD KIT 1000 MAH",
+      price: 190,
+      oldPrice: 255,
+      quantity: 1,
+      image: image, // replace with your actual image path
+    },
+    {
+      id: 2,
+      name: "GEEK VAPE H1 POD KIT 1000 MAH",
+      price: 190,
+      oldPrice: 255,
+      quantity: 1,
+      image: image,
+    },
+    {
+      id: 3,
+      name: "GEEK VAPE H1 POD KIT 1000 MAH",
+      price: 190,
+      oldPrice: 255,
+      quantity: 1,
+      image: image,
+    },
+  ]);
 
+  const updateQuantity = (id: any, type: any) => {
+    setCartItems((prev) =>
+      prev.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              quantity:
+                type === "increase"
+                  ? item.quantity + 1
+                  : item.quantity > 1
+                  ? item.quantity - 1
+                  : 1,
+            }
+          : item
+      )
+    );
+  };
 
-    
-  </div>;
+  return (
+    <div className="container mx-auto p-5 ">
+      <div className="flex justify-between items-center my-5">
+        <h1 className="text-lg font-bold">Your Cart</h1>
+        <p className="text-gray-600 font-bold">{cartItems.length} Items</p>
+      </div>
+
+      <div className="grid  grid-cols-3 md:grid-cols-4 text-gray-400 font-medium border-b border-b-gray-400 pb-2 mb-3">
+        <p>Products</p>
+        <p className="text-center">Quantity</p>
+        <p className="hidden md:block text-right col-span-2">Price</p>
+      </div>
+
+      {cartItems.map((item) => (
+        <div
+          key={item.id}
+          className="md:grid grid-cols-3 md:grid-cols-4 md:items-center gap-4 border-b border-b-gray-400 py-4"
+        >
+          <div className="flex items-center gap-3">
+            <Image
+              src={item.image}
+              alt={item.name}
+              width={60}
+              height={60}
+              className="rounded"
+            />
+            <div>
+              <p className="font-medium">{item.name}</p>
+              <div className="flex gap-2 items-center">
+                <span className="text-orange-500 font-semibold">
+                  ${item.price}
+                </span>
+                <span className="text-gray-400 line-through text-sm">
+                  ${item.oldPrice}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex md:items-center justify-center gap-2">
+            <button
+              onClick={() => updateQuantity(item.id, "decrease")}
+              className="border px-2"
+            >
+              −
+            </button>
+            <span className="border px-3 py-1">{item.quantity}</span>
+            <button
+              onClick={() => updateQuantity(item.id, "increase")}
+              className="border px-2"
+            >
+              +
+            </button>
+          </div>
+
+          <div className="hidden md:block col-span-2 text-right font-medium">
+            ${item.price}
+          </div>
+        </div>
+      ))}
+
+      <div className="flex justify-between items-center mt-5">
+        <Link href="/categories">
+          <div className="text-[#3f67bc] font-bold">
+            <button className=" ">← Continue shopping</button>
+          </div>
+        </Link>
+        <Link href="/checkout">
+          <div className="text-white">
+            <button className="bg-[#3f67bc] px-6 py-2 rounded-lg">
+              Checkout
+            </button>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
---
+++ src/app/(withNavFooter)/checkout/page.tsx
@@ -0,0 +1,127 @@
+"use client";
+import React from "react";
+import { ConfigProvider, Form, Input } from "antd";
+import type { FormProps } from "antd";
+
+interface ContactFormValues {
+  name: string;
+  email: string;
+  contact: string;
+  message?: string;
+}
+
+const CheckoutPage: React.FC = () => {
+  const onFinish: FormProps<ContactFormValues>["onFinish"] = (values) => {
+    console.log("Form Submitted:", values);
+  };
+
+  return (
+    <div className="container mx-auto my-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-28">
+        <div>
+          <h1 className="text-2xl font-bold text-neutral-600">Order Summery</h1>
+          <div className="border-b-2 border-b-neutral-300 pb-5">
+            <div className="text-neutral-400 flex justify-between items-center gap-5">
+              <h1 className="">SUBTOTAL</h1>
+              <p>$190</p>
+            </div>
+            <div className="text-neutral-400 flex justify-between items-center gap-5">
+              <h1>SHIPPING</h1>
+              <p>$80</p>
+            </div>
+          </div>
+          <div className="text-neutral-400 flex justify-between items-center gap-5 mt-5">
+            <h1>TOTAL</h1>
+            <p>$200</p>
+          </div>
+        </div>
+        <ConfigProvider
+          theme={{
+            components: {
+              Form: {
+                borderRadius: 0,
+              },
+              Input: {
+                borderRadius: 5,
+              },
+            },
+          }}
+        >
+          <Form<ContactFormValues>
+            name="checkout"
+            initialValues={{}}
+            onFinish={onFinish}
+            layout="vertical"
+            className="py-10 mx-4 md:mx-0 px-6 md:px-10 "
+          >
+            <div className="mb-4 text-center">
+              <h2 className="text-neutral-700 text-xl md:text-2xl lg:text-3xl font-bold mb-6 uppercase">
+                Checkout
+              </h2>
+              <p className="text-neutral-400 lg:text-lg font-bold">
+                Shipping Information
+              </p>
+            </div>
+
+            <Form.Item
+              name="name"
+              label={<p className="text-md">Full Name</p>}
+              rules={[{ required: true, message: "Please enter your name" }]}
+            >
+              <Input placeholder="Your Name" style={{ padding: "6px" }} />
+            </Form.Item>
+
+            <Form.Item
+              name="email"
+              label={<p className="text-md">Email</p>}
+              rules={[
+                { required: true, message: "Please enter your email" },
+                { type: "email", message: "Please enter a valid email" },
+              ]}
+            >
+              <Input placeholder="Your Email" style={{ padding: "6px" }} />
+            </Form.Item>
+
+            <Form.Item
+              name="contact"
+              label={<p className="text-md">Phone Number</p>}
+              rules={[
+                { required: true, message: "Please enter your Phone number" },
+              ]}
+            >
+              <Input
+                type="tel"
+                placeholder="Phone Number"
+                style={{ padding: "6px" }}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="address"
+              label={<p className="text-md">Address</p>}
+            >
+              <Input.TextArea
+                placeholder="Your Address"
+                rows={3}
+                className="w-full rounded-md"
+              />
+            </Form.Item>
+
+            <Form.Item className="text-center">
+              <div className="text-white">
+                <button
+                  type="submit"
+                  className="w-full py-3 font-bold text-2xl bg-[#3f67bc]   rounded-md shadow-lg"
+                >
+                  Pay with stipes
+                </button>
+              </div>
+            </Form.Item>
+          </Form>
+        </ConfigProvider>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutPage;
