85ce70b10a09dd99df93322281e878971088552e
Rabeya Akter <rabeyariya251@gmail.com>
2025-09-11 09:51:14 +0600

    implement limited offer

--- src/app/(withNavFooter)/page.tsx
+++ src/app/(withNavFooter)/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/pages/HomePage/Banner";
 import BestSellers from "@/components/pages/HomePage/BestSellers";
 import FeatureProducts from "@/components/pages/HomePage/FeatureProducts";
+import LimitedOffer from "@/components/pages/HomePage/LimitedOffer";
 import React from "react";
 
 const HomePage = () => {
@@ -8,7 +9,7 @@ const HomePage = () => {
     <div>
       <Banner />
       <FeatureProducts />
-      {/* <LimitedOffer /> */}
+      <LimitedOffer />
       <BestSellers />
     </div>
   );
--- src/components/pages/HomePage/LimitedOffer.tsx
+++ src/components/pages/HomePage/LimitedOffer.tsx
@@ -4,10 +4,20 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import productImg from "../../../assets/image/Rectangle 161.png";
 import Link from "next/link";
+import { useGetBannerQuery } from "@/redux/features/heroApi/heroApi";
 
 const LimitedOffer: React.FC = () => {
+  const { data: infoData } = useGetBannerQuery(undefined);
+
+  const targetDate = infoData?.data?.countDownDate
+    ? new Date(infoData.data.countDownDate).getTime()
+    : null;
+
   const calculateTimeLeft = () => {
-    const targetDate = new Date("2025-08-15T00:00:00").getTime();
+    if (!targetDate) {
+      return { days: "00", hours: "00", minutes: "00", seconds: "00" };
+    }
+
     const now = new Date().getTime();
     const difference = targetDate - now;
 
@@ -35,16 +45,19 @@ const LimitedOffer: React.FC = () => {
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {
+    if (!targetDate) return;
+
     const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
+
     return () => clearInterval(timer);
-  }, []);
+  }, [targetDate]);
 
   return (
     <div className="container mx-auto">
       <div className="flex flex-col md:flex-row items-center justify-between bg-gray-50 p-8 rounded-lg shadow-lg">
-        <div className="mb-6 md:mb-0 ">
+        <div className="mb-6 md:mb-0">
           <h1 className="text-4xl font-bold mb-6">Offer Ends Soon</h1>
 
           <div className="flex flex-col md:flex-row gap-4 my-16">
@@ -60,7 +73,7 @@ const LimitedOffer: React.FC = () => {
           </div>
           <div className="text-white">
             <Link href="/products">
-              <button className="bg-[#3f67bc]  px-6 py-2 rounded-md transition">
+              <button className="bg-[#3f67bc] px-6 py-2 rounded-md transition">
                 Shop Now
               </button>
             </Link>
