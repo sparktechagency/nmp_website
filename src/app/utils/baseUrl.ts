43aaace67014df77886697bab3509aeef67a46e1
Rabeya Akter <rabeyariya251@gmail.com>
2025-09-23 13:21:42 +0600

    update base url

--- src/app/utils/baseUrl.ts
+++ src/app/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL="http://10.10.20.24:9090/api/v1"
-export const BASE_URL="https://nmp-backend.vercel.app/api/v1"
+export const BASE_URL="http://16.171.171.18:9090/api/v1"
+// export const BASE_URL="https://nmp-backend.vercel.app/api/v1"
