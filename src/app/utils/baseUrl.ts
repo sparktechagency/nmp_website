c3779b35daf11b591c27b7eb1fa7aa51ca76cff3
Osman Goni <94566411+goni715@users.noreply.github.com>
2025-10-03 09:34:59 +0600

    Update baseUrl.ts

--- src/app/utils/baseUrl.ts
+++ src/app/utils/baseUrl.ts
@@ -1,2 +1,2 @@
 //export const BASE_URL="http://16.171.171.18:9090/api/v1"
-export const BASE_URL="https://nmp-backend.vercel.app/api/v1"
+export const BASE_URL="https://backend.onlinecornerstore.com/api/v1"
